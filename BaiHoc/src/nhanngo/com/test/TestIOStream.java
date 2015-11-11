a4b7bdeaf00dcae68e5de2dcfa944afb71bd4647
nhanndtvn <thunhann@yahoo.com>
2015-11-11 08:09:00 +0700

    Discard
    Discard

--- BaiHoc/src/nhanngo/com/test/TestIOStream.java
+++ BaiHoc/src/nhanngo/com/test/TestIOStream.java
@@ -8,7 +8,6 @@ public class TestIOStream {
 		ReadWriteFile objRead=new ReadWriteFile();
 		objRead.readFile("D:/Test.txt");
 		
-		objRead.writeFile("D:/Test2.txt", "Ghi lai lan 2!");
 		System.out.println("Noi dung da ghi");
 		objRead.readFile("D:/Test2.txt");
 	}
